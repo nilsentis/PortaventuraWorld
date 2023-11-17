0d48a8f22272a9afe4ce7ee6065f0905e1f10280
Alexrupa27 <114150062+Alexrupa27@users.noreply.github.com>
2023-11-17 12:26:16 +0100

    Transport inici

--- app/src/main/java/com/example/portaventuraworld/TransportActivity.java
+++ app/src/main/java/com/example/portaventuraworld/TransportActivity.java
@@ -2,13 +2,29 @@ package com.example.portaventuraworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class TransportActivity extends AppCompatActivity {
-
+    ImageView bReturn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transport);
+
+        bReturn = findViewById(R.id.Atras);
+        bReturn.setOnClickListener(this);
+
     }
+    @Override
+    public void onClick(View view) {
+
+        if (view.getId()==R.id.Atras)
+        {
+            Intent intent = new Intent (this, MainActivity.class);
+            startActivity(intent);
+        }
+
 }
