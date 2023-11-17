fbc3068339b1f452bbded9717906a46b249b2fc0
nilsentis <nil.sentisb@educem.net>
2023-11-17 12:21:50 +0100

    2n

--- app/src/main/java/com/example/portaventuraworld/RestaurantsActivity.java
+++ app/src/main/java/com/example/portaventuraworld/RestaurantsActivity.java
@@ -2,13 +2,29 @@ package com.example.portaventuraworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class RestaurantsActivity extends AppCompatActivity {
+public class RestaurantsActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Button breturn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
+
+        breturn = findViewById(R.id.bReturn);
+        breturn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId()==R.id.bReturn)
+        {
+            Intent intent = new Intent (this, MainActivity.class);
+            startActivity(intent);
+        }
     }
 }
